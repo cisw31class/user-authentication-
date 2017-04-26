f3d73075f7479599583ff467caf2ee9214666448
root <root@localhost.localdomain>
2017-04-25 21:46:25 -0700

    Beginning of app

--- Register.php
+++ Register.php
@@ -6,12 +6,18 @@
     <body>
         <h1>Register Page</h1>
 
-            <form action="db_connect/db_connect.php" method="POST">
+            <form action="insert_user/createUser.php" method="POST">
                 <label>Name:</label><br>
                 <input type="text" name="name">
                 <br><br>
+                <label>Email:</label><br>
+                <input type="text" name="email">
+                <br><br>
                 <label>Password:</label><br>
                 <input type="password" name="password">
+                <br><br>
+                <label>Confirm Password:</label><br>
+                <input type="password" name="confirmPassword">
                 <br><br>
                 <input type="submit" name="submit">
             </form>
--- db_connect/db_connect.php
+++ db_connect/db_connect.php
@@ -1,24 +1,12 @@
 <?php
 
 function db_connect() {
-   $result = new mysqli('localhost', 'admin', 'cisw31', 'finalproject');
-   if (!$result) {
-     throw new Exception('Could not connect to database server');
-   } else {
-	echo"CONNECTION SUCCESSFUL";
-     return $result;
-   }
+   $result = new mysqli('127.0.0.1', 'admin', 'cisw31', 'finalproject');
+   if($result){
+       echo"DB CONNECTED";
+       }
 }
 
-function lamp_connect() {
-   $result = new mysqli('localhost', 'admin', 'cisw31', 'finalproject');
-   if (!$result) {
-     throw new Exception('Could not connect to database server');
-   } else {
-	echo"CONNECTION SUCCESSFUL";
-     return $result;
-   }
-}
 
 
 
