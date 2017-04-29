9931c89586b12f30c030d31f4ff407e4ee25266f
Daniel Ocean <ocean@Daniels-MacBook-Pro.local>
2017-04-28 19:26:57 -0700

    project layout

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE HTML>
+<!--
+	Exponent by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+-->
+<html>
+	<head>
+		<title>Nerd College</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="https://github.com/cisw31class/user-authentication-">Nerds in the cloud</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">Log In</a></li>
+						<li class="special">
+							<a href="#menu" class="icon fa-bars">Menu</a>
+							<div id="menu">
+								<ul>
+									<li><a href="#">Home</a></li>
+									<li><a href="profile.php">Profile</a></li>
+									<li><a href="discussions.php">Discussions</a></li>
+									<li><a href="contact.php">Contact</a></li>
+									<li>
+										<span>Status</span>
+										<ul>
+											<li><a href="#">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h2>Nerd College</h2>
+					<ul class="actions">
+						<li><a href="#" class="button big special">Sign up!</a></li>
+					</ul>
+					<p class="footnote"><a href="#">Learn more</a></p>
+				</div>
+				<a href="#one" class="more scrolly">Learn more ...</a>
+			</section>
+
+		<!-- Main -->
+			<div id="main" class="container">
+
+				<!-- One -->
+					<section id="one" class="feature major">
+						<span class="icon alt major fa-paper-plane"></span>
+						<h2>We are nerd college and we can help you take over the world.</h2>
+						<p>Our goal is to provide a place where we can all get our nerd on so here it is. This
+						is a place where you can ask questions and have them answered by other nerds who are learning the same thing. All you have to do is create an account and log in. </p>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="feature">
+						<div class="row">
+							<div class="4u 12u$(medium)">
+								<h2>Discussions</h2>
+								<p>We love web technologies and linux so thats what we focus on
+								but if you have other questions, feel free to start new discussions.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">Start new discussion</a></li>
+								</ul>
+							</div>
+							<div class="8u$ 12u$(medium)">
+								<ul class="major-icons row">
+									<li class="4u 6u(small) 12u$(xsmall)">
+										<span class="icon alt major fa-server"></span>
+										<p><a href="">Servers</a></p>
+									</li>
+									<li class="4u 6u$(small) 12u$(xsmall)">
+										<span class="icon alt major fa-linux"></span>
+										<p><a href="#">Linux</a></p>
+									</li>
+									<li class="4u$ 6u(small) 12u$(xsmall)">
+										<span class="icon alt major fa-github"></span>
+										<p><a href="#">Github</a></p>
+									</li>
+									<li class="4u 6u$(small) 12u$(xsmall)">
+										<span class="icon alt major fa-code"></span>
+										<p><a href="#">HTML</a></p>
+									</li>
+									<li class="4u 6u(small) 12u$(xsmall)">
+										<span class="icon alt major fa-database"></span>
+										<p><a href="#">Databases</a></p>
+									</li>
+									<li class="4u$ 6u$(small) 12u$(xsmall)">
+										<span class="icon alt major fa-bolt"></span>
+										<p><a href="#">PHP</a></p>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="feature">
+						<div class="row">
+							<div class="8u 12u$(medium)">
+								<div class="box alt">
+									<div class="row uniform 50%">
+										<div class="6u 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of nerdstuff</h3>
+												<img src="images/pic01.jpg" alt="" />
+											</a>
+										</div>
+										<div class="6u$ 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of a really nice comp setup</h3>
+												<img src="images/pic02.jpg" alt="" />
+											</a>
+										</div>
+										<div class="6u 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of our team</h3>
+												<img src="images/pic03.jpg" alt="" />
+											</a>
+										</div>
+										<div class="6u$ 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of servers</h3>
+												<img src="images/pic04.jpg" alt="" />
+											</a>
+										</div>
+										<div class="6u 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of us doing homework/paperwork</h3>
+												<img src="images/pic05.jpg" alt="" />
+											</a>
+										</div>
+										<div class="6u$ 12u$(xsmall)">
+											<a href="#" class="image fit">
+												<h3 class="caption">Picture of future jobs</h3>
+												<img src="images/pic06.jpg" alt="" />
+											</a>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="4u$ 12u$(medium) important(medium)">
+								<h2>Work hard or hardly work</h2>
+								<p>We have had many obstacles and overcome alot of hardships but the thing that keeps us moving forward is the goal we have set for ourselves yesterday. There is no problem too big when you see yourself as the storm instead of fearing it and hiding from it bla bla bla....the user can click on the "more" button and view more pictures of us putting this project together. By the way, this gives me another idea, we should start taking photos of the group everytime we have class, we will need them in the future. Keep it quiet tho so the other guys dont steal our ideas (they are doing the same project).</p>
+
+								<p>The goal is to create a website where people can actually browse and choose what they like (and set bookmarks) instead of just an informational site that does nothing. I was thinking that we can put up different discussions about different topics and this will provide a selection platform for our visitors. Again, this functionality will only be available to those who are logged in.</p>
+								<ul class="actions">
+									<li><a href="#" class="button">MORE PHOTOS</a></li>
+								</ul>
+							</div>
+						</div>
+					</section>
+
+				<!-- Four -->
+					<section id="four" class="feature major">
+						<h2>Right this way...</h2>
+						<p>Clicking the "Join now" button will take the visitor to a page where he can create an account. I think the page should be simple, only asking for a name, email and password (and ofcourse confirm password).</p>
+						<ul class="actions">
+							<li><a href="#" class="button big special">Join now!</a></li>
+						</ul>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<section class="4u 12u$(medium) about">
+							<h2>Did you know?</h2>
+							<p>Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful.</p>
+							<ul class="actions">
+								<li><a href="#" class="button">Read the story</a></li>
+							</ul>
+						</section>
+						<section class="4u 6u(medium) 12u$(small)">
+							<h3>Updates</h3>
+							<ul class="updates">
+								<li>
+									<p><a href="#">This can be static text, it would serve as a decent place holder.</a></p>
+									<span class="timestamp">Posted 2 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Also, static...maybe some news story that just happened</a></p>
+									<span class="timestamp">Posted 4 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Our goal is to focus on server side functionality, so lets just fill this portion in with static text also.</a></p>
+									<span class="timestamp">Posted 8 hours ago</span>
+								</li>
+							</ul>
+						</section>
+						<section class="4u$ 6u$(medium) 12u$(small)">
+							<h3>Get in touch</h3>
+							<ul class="labeled-icons">
+								<li>
+									<h4 class="icon alt fa-home"><span class="label">Address</span></h4>
+									<span>1100 N Grand Ave<br />
+									Walnut, CA 91789</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-phone"><span class="label">Phone</span></h4>
+									<span>(800) 555-5555</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-envelope"><span class="label">Email</span></h4>
+									<a href="#">example@example.com</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-facebook"><span class="label">Facebook</span></h4>
+									<a href="#">facebook.com/untitled-tld</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-twitter"><span class="label">Twitter</span></h4>
+									<a href="#">twitter.com/untitled-tld</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+				<div class="copyright container">
+					<p>&copy; NerdCollege, All rights reserved.</p>
+					<ul class="menu">
+						<li><a href="#">Terms of Use</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,182 @@
+<!DOCTYPE HTML>
+<!--
+	Exponent by Pixelarity
+	pixelarity.com | hello@pixelarity.com
+	License: pixelarity.com/license
+-->
+<html>
+	<head>
+		<title>Dynamic Title</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+
+		<!--BOOTSTRAP w/ Jquery CORE-->
+		<script src="bootstrap/jquery-3.2.1.js"></script>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+		<script src="bootstrap/js/bootstrap.js"></script>
+		<!--BOOTSTRAP w/ Jquery CORE-->
+
+		<style>
+			#profile_pic {
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<h1><!--PLACEHOLDER--></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">Log In</a></li>
+						<li class="special">
+							<a href="#menu" class="icon fa-bars">Menu</a>
+							<div id="menu">
+								<ul>
+									<li><a href="index.php">Home</a></li>
+									<li><a href="#">Profile</a></li>
+									<li><a href="discussions.php">Discussions</a></li>
+									<li><a href="contact.php">Contact</a></li>
+									<li>
+										<span>Status</span>
+										<ul>
+											<li><a href="#">Sign Up</a></li>
+											<li><a href="#">Log In</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Main -->
+			<div id="main" class="container">
+				<header class="major">
+					<h2>Profile</h2>
+				</header>
+
+				<!-- Content -->
+					<section id="content">
+						<div class="container">
+							<div class="row" align="center">
+								<div class="col-md-12">
+									<h3>Welcome... "name of user displayed dynamically"</h3><br><br><br>
+								</div>	
+							</div>
+
+							<div class="row">
+								<div class="col-md-4">
+									<!--PLACEHOLDER-->
+								</div>
+
+								<div class="col-md-4">
+									<div id="profile_pic">
+										<img src="images/avatar.png" class="img-circle" height="200" width="200">
+									</div>
+
+									<br><br>
+
+									<form action="" method="" enctype="multipart/form-data">
+										<input type="file" name="user_photo"><br><br>
+
+										<input type="text" name="username" placeholder="Username" class="form-control"><br>
+										<input type="text" name="email" placeholder="Email" class="form-control"><br>
+										<input type="password" name="password" placeholder="Password" class="form-control"><br>
+										<input type="password" name="confirm_password" placeholder="Confirm Password" class="form-control"><br>	
+										<input type="submit" name="submit" value="Update">
+									</form>
+								</div>
+
+								<div class="col-md-4">
+									<!--PLACEHOLDER-->
+								</div>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<section class="4u 12u$(medium) about">
+							<h2>Did you know?</h2>
+							<p>Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful. Someone can fill this text with something useful.</p>
+							<ul class="actions">
+								<li><a href="#" class="button">Read the story</a></li>
+							</ul>
+						</section>
+						<section class="4u 6u(medium) 12u$(small)">
+							<h3>Updates</h3>
+							<ul class="updates">
+								<li>
+									<p><a href="#">This can be static text, it would serve as a decent place holder.</a></p>
+									<span class="timestamp">Posted 2 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Also, static...maybe some news story that just happened</a></p>
+									<span class="timestamp">Posted 4 hours ago</span>
+								</li>
+								<li>
+									<p><a href="#">Our goal is to focus on server side functionality, so lets just fill this portion in with static text also.</a></p>
+									<span class="timestamp">Posted 8 hours ago</span>
+								</li>
+							</ul>
+						</section>
+						<section class="4u$ 6u$(medium) 12u$(small)">
+							<h3>Get in touch</h3>
+							<ul class="labeled-icons">
+								<li>
+									<h4 class="icon alt fa-home"><span class="label">Address</span></h4>
+									<span>1100 N Grand Ave<br />
+									Walnut, CA 91789</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-phone"><span class="label">Phone</span></h4>
+									<span>(800) 555-5555</span>
+								</li>
+								<li>
+									<h4 class="icon alt fa-envelope"><span class="label">Email</span></h4>
+									<a href="#">example@example.com</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-facebook"><span class="label">Facebook</span></h4>
+									<a href="#">facebook.com/untitled-tld</a>
+								</li>
+								<li>
+									<h4 class="icon alt fa-twitter"><span class="label">Twitter</span></h4>
+									<a href="#">twitter.com/untitled-tld</a>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+				<div class="copyright container">
+					<p>&copy; NerdCollege, All rights reserved.</p>
+					<ul class="menu">
+						<li><a href="#">Terms of Use</a></li>
+						<li><a href="#">Privacy Policy</a></li>
+						<li><a href="#">Contact Us</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
